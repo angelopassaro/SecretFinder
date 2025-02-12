2a257530b481c62d41841181cd70251d8d987230
angelopassaro <angelopassaro1@gmail.com>
2025-02-12 08:32:37 -0500

    Fix setup

--- setup.py
+++ setup.py
@@ -1,32 +1,13 @@
-#!/usr/bin/env python
 from setuptools import setup, find_packages
 
 setup(
-    name='SecretFinder',
-    version='1.0.0',
-    description='Tool for discovering API keys, access tokens, and sensitive data in JS files.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='m4ll0k',
-    author_email='m4ll0k2@gmail.com',
-    url='https://github.com/angelopassaro/SecretFinder',
-    packages=find_packages(),
-    install_requires=[
-        'requests',              # Required for HTTP requests
-        'requests-file',         # Support for reading local files with file://
-        'lxml',                  # Required for parsing HTML
-        'jsbeautifier',          # Beautifier for JS files
-    ],
-    entry_points={
+    name="secretfinder", 
+    version="1.0.0",  
+    packages=find_packages(),  
+    install_requires=open('requirements.txt').read().splitlines(), 
+    entry_points={  
         'console_scripts': [
-            'secretfinder = secretfinder:main',
+            'secretfinder = secretfinder.main:main',  
         ],
     },
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',  # Assumes Python 3.6 or higher
-    include_package_data=True,
 )
