3039d762c62de6a814ae3a975f98f5cc91f48c3c
Angelo Passaro <14933081+angelopassaro@users.noreply.github.com>
2025-02-12 12:36:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,12 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 
 setup(
     name='SecretFinder',
-    version='1.0.0',
-    description='Tool for discovering API keys, access tokens, and sensitive data in JS files.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='m4ll0k',
-    author_email='m4ll0k2@gmail.com',
-    url='https://github.com/angelopassaro/SecretFinder',
     packages=find_packages(),
-    install_requires=[
-        'requests',              # Required for HTTP requests
-        'requests-file',         # Support for reading local files with file://
-        'lxml',                  # Required for parsing HTML
-        'jsbeautifier',          # Beautifier for JS files
-    ],
-    entry_points={
-        'console_scripts': [
-            'secretfinder = SecretFinder:main',
-        ],
-    },
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',  # Assumes Python 3.6 or higher
-    include_package_data=True,
+    version='1.0',
+    long_description=open('README.md').read(),
+    url='https://github.com/angelopassaro/SecretFinder',
+    py_modules=['secretfinder'],
+    install_requires=['requests','requests-file','lxml','jsbeautifier'],
 )
